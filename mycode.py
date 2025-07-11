2f1c49e9ee69eef05bd474f56f696fe81f45f601
Amit <amitdutta121@gmail.com>
2025-07-11 13:55:23 -0700

    Data changed in DVC

--- mycode.py
+++ mycode.py
@@ -16,6 +16,13 @@ new_row_loc = {
     'City': 'San Francisco'
 }
 df.loc[len(df.index)] = new_row_loc
+new_row_loc2 = {
+    'Name': 'Eve',
+    'Age': 22,
+    'City': 'Seattle'
+}
+df.loc[len(df.index)] = new_row_loc2
+
 
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
